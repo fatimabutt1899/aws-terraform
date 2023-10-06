fa88535247efe682243debf0587748692960cf1c
Fatima Butt <fatimabutt18899@gmail.com>
2023-10-06 15:34:10 +0500

    added elastic beanstakl configuration

---
+++ Elastic-Beanstalk/var.tf
@@ -0,0 +1,19 @@
+variable "elasticapp" {
+  default = "Elastic-Beanstalk-Test-App"
+}
+variable "beanstalkappenv" {
+  default = "Elastic-Beanstalk-Env"
+}
+variable "solution_stack_name" {
+  default = "64bit Amazon Linux 2 v5.0.0 running Python 3.8"
+}
+variable "tier" {
+  default = "WebServer"
+}
+ 
+variable "vpc_id" {
+    default = "vpc-0aaf2aa25ad0ea661"
+}
+variable "public_subnets" {
+    default = ["subnet-0bb5ec94ca757154b", "subnet-0ad361637de63e0d3"]
+}
